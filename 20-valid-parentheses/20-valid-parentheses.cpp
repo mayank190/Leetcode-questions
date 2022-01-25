acb8cdf4c17620ecbb75debb08af1f999dbf0c05
mayank190 <77227296+mayank190@users.noreply.github.com>
2022-01-25 19:04:14 +0530

    Time: 5 ms (11.75%), Space: 6.5 MB (9.83%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,22 @@
+unordered_map<char,int> symbols={{'{',-1},{'(',-2},{'[',-3},{'}',1},{')',2},{']',3}};
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(char &bracket:s){
+            if(symbols[bracket]<0){
+                st.push(bracket);
+            }
+            else{
+                if(st.empty())return 0;
+                char top=st.top();
+                st.pop();
+                if(symbols[top]+symbols[bracket]!=0)
+                return 0;
+            }
+        }
+        if(st.empty())
+        return 1;
+        return 0;
+    }
+};
